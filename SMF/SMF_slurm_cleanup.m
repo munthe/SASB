59bc0cc26ee8ebd3366cecab0b1fc6d2b4ae5a28
Emil Munthe <emil@munthe.dk>
2014-03-07 19:03:26 +0100

    clean up working for merging the filter.
    Not tested for bigge amounts of data

--- SMF/SMF_slurm_cleanup.m
+++ SMF/SMF_slurm_cleanup.m
@@ -2,14 +2,14 @@
 % 
 
 %% Load filter lines
-SMF = cell(resolution(1),length(par.scanline));
-for line = par.scanline
+SMF = cell(resolution(1),length(par.line));
+for line = par.line
     load([savepath tmpdir 'SMF_line_' num2str(line)]);
     SMF(:,line) = SMFline;
 end
 
 %% Save filter
-save([savepath 'SMF'],SMF)
+save([savepath 'SMF'],'SMF')
 
 %% Clean up
 rmdir([savepath tmpdir],'s')
