2575d22c00851e28d654dc8d787fdf34900877c3
Emil Munthe <emil@munthe.dk>
2014-03-31 17:38:42 +0200

    adjustments to font sizes and colors

--- Abstract/psf_plot.m
+++ Abstract/psf_plot.m
@@ -5,7 +5,7 @@
 base_path = './';
 loadpath = './';
 base_path_usecase = './';
-
+addpath(genpath('../lib'))
 savepath = loadpath;
 savepath_figures = savepath;
 figure_format = '-depsc2';
@@ -15,21 +15,32 @@ setupfigdefault
 load psf
 
 %% Plot psf as function of depth
-
-fig_nr = 1;
-figure(fig_nr);
 n = 1:7;
 
-depth = cell2mat({psf_SMF(n).y_coord})*1000;
+fig_nr = 1;
+set(0, 'DefaultAxesColorOrder', linspecer(2));
+figure(fig_nr);
+depth = cell2mat({psf_SMF(n).y_coord});
 plot( ...
-    depth,cell2mat({psf_SMF(n).fwhm_x}),'o--', ...
-    depth,cell2mat({psf_SASB(n).fwhm_x}),'d--', ...
-    depth,cell2mat({psf_SMF(n).radius20dB}),'o:', ...
-    depth,cell2mat({psf_SASB(n).radius20dB}),'d:' ...
+    depth,cell2mat({psf_SMF(n).fwhm_x}),'s-', ...
+    depth,cell2mat({psf_SASB(n).fwhm_x}),'o-', ...
+    depth,cell2mat({psf_SMF(n).radius20dB}),'s--', ...
+    depth,cell2mat({psf_SASB(n).radius20dB}),'o--',...
+    'MarkerSize',6, ...
+    'LineWidth',1.5 ...
     );
 xlabel('Depth of point scatter [mm]');
 ylabel('[mm]');
-legend('FWHM SMF','FWHM SASB','Radius 20dB SMF','Radius 20dB SASB','Location','NorthWest');
+legend('FWHM SMF','FWHM SASB','R\_20dB SMF','R\_20dB SASB','Location','NorthWest');
 % title(psf(setting,1).setupDesc);
 prettyfig
 print(figure(fig_nr),[savepath_figures 'psf'],figure_format)
+hold off
+
+%% Calculate percentages
+
+mean(cell2mat({psf_SMF(n).fwhm_x})./cell2mat({psf_SASB(n).fwhm_x}))
+mean(cell2mat({psf_SMF(n).fwhm_x}))/mean(cell2mat({psf_SASB(n).fwhm_x}))
+
+mean(cell2mat({psf_SMF(n).radius20dB})./cell2mat({psf_SASB(n).radius20dB}))
+mean(cell2mat({psf_SMF(n).radius20dB}))/mean(cell2mat({psf_SASB(n).radius20dB}))
