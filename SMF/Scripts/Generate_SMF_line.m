17c7eedf6514a602aef9de08dc4329796e52f363
Emil Munthe <emil@munthe.dk>
2014-04-04 11:39:42 +0200

    Option to chance filter cropping threshold

--- SMF/Scripts/Generate_SMF_line.m
+++ SMF/Scripts/Generate_SMF_line.m
@@ -5,8 +5,10 @@
 % x_coord = x coordinate for line to generate 
 % useCaseParams
 % transducerType
+% crop threshold in dB, default = -20dB
 
-function SMF = Generate_SMF_line(x_coord,resolution,useCaseParams,transducerType)
+function SMF = Generate_SMF_line(x_coord,resolution,useCaseParams,transducerType,cropdB)
+if nargin < 5, cropdB = -20; end
 
 y_tismin = useCaseParams.scanparams(1).windowtissueq.y_tismin;
 if y_tismin<1/1000; y_tismin = 1/1000; end % Scatter cannot be at depth less than 1mm
@@ -16,7 +18,7 @@ z = linspace(y_tismin,...
 % Generate cellarray with positions for point scatteres
 position = num2cell([x_coord*ones(size(z)) zeros(size(z)) z],2);
 [filter,index] = cellfun( @(X) ...
-    Generate_SMF_point(X,useCaseParams,transducerType),...
+    Generate_SMF_point(X,useCaseParams,transducerType,cropdB),...
     position,...
     'UniformOutput',false);
 SMF = struct('filter',filter,'index',index);
@@ -28,7 +30,7 @@ SMF = struct('filter',filter,'index',index);
 
 end
 
-function [SMF,index] = Generate_SMF_point(position,useCaseParams,transducerType)
+function [SMF,index] = Generate_SMF_point(position,useCaseParams,transducerType,cropdB)
 
 media.phantom_positions = position;
 media.phantom_amplitudes = 1;
@@ -53,7 +55,7 @@ SMF = Data_Acquisition('usecaseparams',useCaseParams, ...
                       'symmetric','symmetric',...
                       'media',media);
 
-SMF = cropfilter(SMF,-20);
+SMF = cropfilter(SMF,cropdB);
 [SMF,index] = removezeros(SMF);
 
 end
